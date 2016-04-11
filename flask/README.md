b1d03ea7abd43a92cd2d8e679ecabd5a582455bf
Neeraj Lad <neeraj.lad@yandex.com>
2016-04-11 18:13:51 +0530

    Complete Part 2

--- flask/README.md
+++
@@ -1,2 +0,0 @@
-# microblog
-Attempt to create a microblog using Miguel Grinberg's Flask mega [tutorial](http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world)
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,6 +1,33 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'} # mock user
+    posts =  [   # mock array of posts
+            {
+                'author': {'nickname': 'John'},
+                'body': 'Beautiful day!'
+                },
+            {
+                'author': {'nickname': 'Susan'},
+                'body': 'Today I Learned'
+                },
+            {
+                'author': {'nickname': 'James'},
+                'body': 'Baaaah..'
+                },
+            {
+                'author': {'nickname': 'Cris'},
+                'body': 'Me, me, me!'
+                },
+            {
+                'author': {'nickname': 'Flo'},
+                'body': 'Hahahoho'
+                }
+            ] 
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            posts=posts)
